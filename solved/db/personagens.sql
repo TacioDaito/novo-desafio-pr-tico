ed18dcfe846904f881ccf4b0934c021cc7c8b8af
TacioDaito <taciokikuchi@gmail.com>
2022-10-25 22:04:16 -0300

    Separating the problem from the solution.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	renamed:    .eslintrc.js -> original/.eslintrc.js
    	renamed:    assets/The-Boys-segunda-temporada-scaled.jpg -> original/assets/The-Boys-segunda-temporada-scaled.jpg
    	renamed:    assets/logo_cotic.fw.png -> original/assets/logo_cotic.fw.png
    	renamed:    backend_php/api/personagens.php -> original/backend_php/api/personagens.php
    	renamed:    backend_php/api/registrar.php -> original/backend_php/api/registrar.php
    	renamed:    backend_php/classes/Personagem.php -> original/backend_php/classes/Personagem.php
    	renamed:    backend_php/classes/SQLDriver.php -> original/backend_php/classes/SQLDriver.php
    	renamed:    css/main.css -> original/css/main.css
    	renamed:    db/personagens.sql -> original/db/personagens.sql
    	renamed:    js/tabela.js -> original/js/tabela.js
    	renamed:    package-lock.json -> original/package-lock.json
    	renamed:    package.json -> original/package.json
    	renamed:    pages/formulario.html -> original/pages/formulario.html
    	renamed:    pages/index.html -> original/pages/index.html
    	renamed:    pages/lista.html -> original/pages/lista.html
    	new file:   solved/.eslintrc.js
    	new file:   solved/assets/The-Boys-segunda-temporada-scaled.jpg
    	new file:   solved/assets/logo_cotic.fw.png
    	new file:   solved/backend_php/api/personagens.php
    	new file:   solved/backend_php/api/registrar.php
    	new file:   solved/backend_php/classes/Personagem.php
    	new file:   solved/backend_php/classes/SQLDriver.php
    	new file:   solved/css/main.css
    	new file:   solved/db/personagens.sql
    	new file:   solved/js/tabela.js
    	new file:   solved/package-lock.json
    	new file:   solved/package.json
    	new file:   solved/pages/formulario.html
    	new file:   solved/pages/index.html
    	new file:   solved/pages/lista.html

---
+++ solved/db/personagens.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 01-Jul-2021 às 21:07
+-- Versão do servidor: 10.1.8-MariaDB
+-- PHP Version: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `desafio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `personagens`
+--
+
+CREATE TABLE `personagens` (
+  `nome` text NOT NULL,
+  `idade` int(11) NOT NULL,
+  `interprete` text NOT NULL,
+  `alinhamento` text NOT NULL,
+  `biografia` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
